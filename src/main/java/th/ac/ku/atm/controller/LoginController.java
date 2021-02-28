dbaeddc7e9b05f6812af8a29fd502d1d69ab313d
eakeakarat <eakarat.sa@ku.th>
2021-02-28 11:39:14 +0700

    add bank account API

--- src/main/java/th/ac/ku/atm/controller/LoginController.java
+++ src/main/java/th/ac/ku/atm/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.BankAccountService;
 import th.ac.ku.atm.service.CustomerService;
 
 @Controller
@@ -14,9 +15,11 @@ import th.ac.ku.atm.service.CustomerService;
 public class LoginController {
 
     private final CustomerService customerService;
+    private BankAccountService bankAccountService;
 
-    public LoginController(CustomerService customerService) {
+    public LoginController(CustomerService customerService, BankAccountService bankAccountService) {
         this.customerService = customerService;
+        this.bankAccountService = bankAccountService;
     }
 
     @GetMapping
@@ -26,14 +29,18 @@ public class LoginController {
 
     @PostMapping
     public String login(@ModelAttribute Customer customer, Model model) {
-        Customer matchingCustomer = customerService.checkPin(customer);
-        if (matchingCustomer != null) {
-            model.addAttribute("greeting",
-                    "Welcome, " + matchingCustomer.getName());
+        Customer storedCustomer = customerService.checkPin(customer);
+
+        if (storedCustomer != null) {
+            model.addAttribute("customertitle", storedCustomer.getName() + " Bank Accounts");
+            model.addAttribute("bankaccounts",
+                    bankAccountService.getCustomerBankAccount(customer.getId()));
+            return "customeraccount";
         } else {
             model.addAttribute("greeting", "Can't find customer");
+            return "home";
         }
-        return "home";
+
     }
 
 }
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,17 +1,26 @@
 package th.ac.ku.atm.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
     private List<BankAccount> bankAccountList;
+    private RestTemplate restTemplate;
+
+    public BankAccountService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
 
     @Autowired
     private CustomerService customerService;
@@ -36,6 +45,17 @@ public class BankAccountService {
             }
         }
         return null;
+    }
+
+    public List<BankAccount> getCustomerBankAccount(int customerId) {
+        String url = "http://localhost:8091/api/bankaccount/customer/" +
+                customerId;
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+
+        return Arrays.asList(accounts);
     }
 
     public Customer checkCustomer(int customerId) {
